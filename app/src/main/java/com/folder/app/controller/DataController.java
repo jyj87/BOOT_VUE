f71fa8983f28309e08d2c62e69fece6e964631c5
jyj <g.jyj87@gmail.com>
2023-11-25 10:29:54 +0900

    공부중

--- app/src/main/java/com/folder/app/controller/DataController.java
+++ app/src/main/java/com/folder/app/controller/DataController.java
@@ -1,18 +1,20 @@
 package com.folder.app.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DataController {
 
+    @CrossOrigin(origins = "http://localhost:8800")
     @GetMapping("/")
     public String home() {
         return "Data 준비중....";
     }
 
-    @GetMapping("/1")
-    public String home1() {
-        return "Data1 준비중....";
+    @GetMapping("/api")
+    public String api() {
+        return "api 준비중....";
     }
 }
